300d461eb76b9bb00ae8d4e367e9419a5f5e5086
ryonakajima-25 <57983611+ryonakajima-25@users.noreply.github.com>
2020-05-24 13:28:45 +0900

    add flash message

--- app/assets/javascripts/time.js
+++ app/assets/javascripts/time.js
@@ -1,7 +1,13 @@
-console.log('time')
 time();
-function time(){
+function time() {
     var now = new Date();
     document.getElementById("time").innerHTML = now.toLocaleTimeString();
-}
+};
 setInterval('time()',1000);
+
+
+function fadeOut() {
+    var flash = document.getElementById("flash")
+    flash.style.opacity = 0;
+};
+setTimeout(fadeOut, 3000);
--- app/controllers/jobTimes_controller.rb
+++ app/controllers/jobTimes_controller.rb
@@ -6,15 +6,22 @@ class JobTimesController < ApplicationController
   def create
     @user = current_user
     @jobTime = @user.job_time.create(startTime: Time.now)
-    @user.update(status: true)
-    redirect_to user_path(current_user)
+    if @user.update(status: true)
+      redirect_to user_path(current_user), notice: "︎打刻しました"
+    else
+      redirect_to user_path(current_user), notice: "︎打刻できませんでした"
+    end
+
   end
 
   def update
     @user = current_user
     @jobTime = @user.job_time
     @jobTime.update(endTime: Time.now)
-    @user.update(status: false)
-    redirect_to user_path(current_user)
+    if @user.update(status: false)
+      redirect_to user_path(current_user), notice: "︎打刻しました"
+    else
+      redirect_to user_path(current_user), notice: "︎打刻できませんでした"
+    end
   end
 end
